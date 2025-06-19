63e37ba460e2a97a2c99750fec47a0c1f7a1b9c1
jabezdubale <jabezdubale6@gmail.com>
2025-06-19 17:23:35 +0300

    1. Implement Local Storage for the To-Do List

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
+  let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
   const addButton = document.getElementById("add-task-btn");
   const taskInput = document.getElementById("task-input");
   const taskList = document.getElementById("task-list");
+
+  function loadLocalTask() {
+    tasks.forEach((value) => {
+      createTaskElement(value);
+    });
+  }
+  loadLocalTask();
 
   function addTask() {
     let taskText = taskInput.value.trim();
@@ -15,15 +23,38 @@ document.addEventListener("DOMContentLoaded", () => {
     const delBut = document.createElement("button");
     delBut.textContent = "Remove";
     delBut.classList.add("remove-btn");
+    tasks.push(taskText);
+    localStorage.setItem("tasks", JSON.stringify(tasks));
 
     delBut.addEventListener("click", () => {
       li.remove();
+      delLocalTask(taskText);
     });
 
     li.appendChild(delBut);
     taskList.appendChild(li);
     taskInput.value = "";
   }
+
+  function createTaskElement(tasks) {
+    const li = document.createElement("li");
+    li.textContent = tasks;
+    const delBut = document.createElement("button");
+    delBut.textContent = "Remove";
+    delBut.classList.add("remove-btn");
+    li.appendChild(delBut);
+    taskList.appendChild(li);
+  }
+
+  function delLocalTask(task) {
+    tasks = tasks.filter((value) => {
+      return value !== task;
+    });
+
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }
+
+  console.log(tasks);
 
   addButton.addEventListener("click", addTask);
   taskInput.addEventListener("keypress", (event) => {
